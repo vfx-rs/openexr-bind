c206fe6fed4357349cb50a8c3d4fdeba0df64928
Luke Titley <from+badja@luketitley.com>
2021-06-20 16:19:55 +0200

    Add hand written build.rs and CMakeLists.txt to scripts folder

---
+++ scripts/build.rs
@@ -0,0 +1,41 @@
+fn build_openexr() -> std::string::String {
+    if let Ok(openexr_root) = std::env::var("OPENEXR_ROOT") {
+        openexr_root
+    } else {
+        println!("OPENEXR_ROOT is not set, building bundled version.");
+
+        // We need to set this to Release or else the openexr symlinks will be incorrect.
+        // Fixed by
+        cmake::Config::new("thirdparty/openexr")
+            .profile("Release")
+            .build()
+            .to_str()
+            .expect("Unable to convert openexr_root to str").to_string()
+    }
+}
+
+fn main() {
+    let openexr_root = build_openexr();
+
+    // Build openexr-c
+    let dst = cmake::Config::new("openexr-c")
+        .define(
+            "OPENEXR_ROOT",
+            &openexr_root,
+        )
+        .build();
+
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=dylib=openexr-c-3_0");
+    println!(
+        "cargo:rustc-link-search=native={}/lib",
+        &openexr_root
+    );
+    println!("cargo:rustc-link-lib=dylib=OpenEXR-3_0");
+    println!("cargo:rustc-link-lib=dylib=OpenEXRUtil-3_0");
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-lib=dylib=c++");
+}
