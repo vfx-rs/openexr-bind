4bfe66c1bf084cf0b6d929866a6622b891b3199d
Luke Titley <from+badja@luketitley.com>
2021-06-21 19:59:59 +0200

    Build Imath if IMATH_ROOT is not defined

--- scripts/build.rs
+++ scripts/build.rs
@@ -1,13 +1,35 @@
+cmake -DCMAKE_PREFIX_PATH=/home/luke/packages/openexr_3.0.4 ../
+
+fn build_imath() -> std::string::String {
+    if let Ok(imath_root) = std::env::var("IMATH_ROOT") {
+        imath_root
+    } else {
+        println!("IMATH_ROOT is not set, building bundled version.");
+
+        // We need to set this to Release or else the openexr symlinks will be incorrect.
+        // Fixed by
+        cmake::Config::new("thirdparty/Imath")
+            .profile("Release")
+            .build()
+            .to_str()
+            .expect("Unable to convert imath_root to str").to_string()
+    }
+}
+
 fn build_openexr() -> std::string::String {
     if let Ok(openexr_root) = std::env::var("OPENEXR_ROOT") {
         openexr_root
     } else {
         println!("OPENEXR_ROOT is not set, building bundled version.");
 
+        // Build imath if we can't find it.
+        let imath_root = build_imath();
+
         // We need to set this to Release or else the openexr symlinks will be incorrect.
         // Fixed by
         cmake::Config::new("thirdparty/openexr")
             .profile("Release")
+            .define("CMAKE_PREFIX_PATH", &imath_root)
             .build()
             .to_str()
             .expect("Unable to convert openexr_root to str").to_string()
