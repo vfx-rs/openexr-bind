41dda81f7c8696055e3c884c3407761be6f53502
Anders Langlands <anderslanglands@gmail.com>
2021-07-05 20:25:17 +1200

    make bind script slightly more reusable

--- bind.sh
+++ bind.sh
@@ -10,17 +10,22 @@ trap 'echo "\"${last_command}\" exited with code $?."' ERR
 
 rm -rf build
 
+project_name="vfx-pre-openexr"
+major_version=0
+minor_version=0
+patch_version=1
+
 astgen bind -v 1 -u -o build/ast -- \
     -I${IMATH_ROOT}/include \
     -I${IMATH_ROOT}/include/Imath \
     -I${OPENEXR_ROOT}/include \
     -I${OPENEXR_ROOT}/include/OpenEXR 
 
-asttoc build/ast -o build -p openexr \
-    -major 3 -minor 0 -patch 1 \
+asttoc build/ast -o build -p ${project_name} \
+    -major ${major_version} -minor ${minor_version} -patch ${patch_version} \
     -L $OPENEXR_ROOT/lib \
     -l OpenEXR-3_0 \
     -l OpenEXRUtil-3_0
 
-cp test.rs build/openexr-sys/src
+cp test.rs build/${project_name}-sys/src
 
